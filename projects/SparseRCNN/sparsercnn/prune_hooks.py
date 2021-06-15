8972a993f5b5af5d50e28619add1dd005cf04fcb
dashu233 <wy-lrc@pku.edu.cn>
2021-06-15 20:09:01 +0800

    prune sparse CNN backbone

---
+++ projects/SparseRCNN/sparsercnn/prune_hooks.py
@@ -0,0 +1,47 @@
+import datetime
+import itertools
+import logging
+import os
+import tempfile
+import time
+from collections import Counter
+import torch
+from fvcore.common.checkpoint import PeriodicCheckpointer as _PeriodicCheckpointer
+from fvcore.common.param_scheduler import ParamScheduler
+from fvcore.common.timer import Timer
+from fvcore.nn.precise_bn import get_bn_modules, update_bn_stats
+
+import detectron2.utils.comm as comm
+from detectron2.evaluation.testing import flatten_results_dict
+from detectron2.solver import LRMultiplier
+from detectron2.utils.events import EventStorage, EventWriter
+from detectron2.utils.file_io import PathManager
+
+from detectron2.engine.train_loop import HookBase
+from detectron2.engine.defaults import DefaultTrainer
+import torch.nn.utils.prune as prune
+
+class UnStructureIMP(HookBase):
+    def __init__(self,prune_steps,prune_gamma,verbose=False):
+        super(UnStructureIMP, self).__init__()
+        self.prune_steps = prune_steps
+        self.prune_gamma = prune_gamma
+        self.param_remain = 1.0
+        self.verbose = verbose
+    def before_train(self):
+        if self.trainer.iter in self.prune_steps:
+            # TODO:switch to global pruning
+            for name, module in self.trainer.trainer_.model.backbone.named_modules():
+                if isinstance(module, torch.nn.Conv2d):
+                    prune.l1_unstructured(module, name='weight', amount=self.prune_gamma)
+                elif isinstance(module, torch.nn.Linear):
+                    prune.l1_unstructured(module, name='weight', amount=self.prune_gamma)
+        self.param_remain *= self.prune_gamma
+
+    def after_train(self):
+        if self.verbose:
+            logger = logging.getLogger(__name__)
+
+            logger.info(
+                "Parameter remain in model: %.2f %%" % self.param_remain * 100
+            )
